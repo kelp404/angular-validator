b1a5db72004c1a97451da5143a9e4300e7d27846
Kelp <kelp@phate.org>
2013-11-01 18:27:08 +0800

    fixed Gruntfile

---
+++ angular-validator.js
@@ -0,0 +1,14 @@
+(function() {
+
+
+}).call(this);
+
+(function() {
+  angular.module('validator', ['validator.provider', 'validator.directive']);
+
+}).call(this);
+
+(function() {
+
+
+}).call(this);
